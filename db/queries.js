817ea2a6e165da287bcc7f9ccfa3f613e6b6885e
S-entinel <106978598+schwarzsch1ld@users.noreply.github.com>
2025-08-20 20:14:58 +0100

    added seeding functionality for comments, articles and users data, and tested with some queries

---
+++ db/queries.js
@@ -0,0 +1,54 @@
+const db = require("./connection.js")
+
+// db.query(
+//     `SELECT * 
+//     FROM users;`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
+
+// db.query(
+//     `SELECT * 
+//     FROM articles
+//     WHERE topic = 'coding';`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
+
+// db.query(
+//     `SELECT * 
+//     FROM comments
+//     WHERE votes < 0;`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
+
+// db.query(
+//     `SELECT * 
+//     FROM topics;`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
+
+// db.query(
+//     `SELECT * 
+//     FROM articles
+//     WHERE author = 'grumpy19'
+//     ;`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
+
+// db.query(
+//     `SELECT * 
+//     FROM comments
+//     WHERE votes > 10;`
+// ).then((data) => {
+//     const result = data.rows
+//     console.log(result)
+// })
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,6 @@
 const db = require("../connection")
 const format = require("pg-format");
+const { convertTimestampToDate, titleToID } = require("./utils.js")
 
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
@@ -62,7 +63,62 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
         RETURNING *;`
       , cleanedTopicData);
       return db.query(insertTopicsStr)
-    }); 
+    })
+    .then(() => {
+      const cleanedUserData = userData.map(
+        ({username, name, avatar_url}) => {
+          return [username, name, avatar_url]
+        })
+
+      const insertUsersStr = format(
+        `INSERT into users
+        (username, name, avatar_url)
+        VALUES %L
+        RETURNING *;`
+      , cleanedUserData);
+      return db.query(insertUsersStr)
+    })
+    .then(() => {
+      const formattedData = articleData.map(convertTimestampToDate)
+
+      const cleanedArticleData = formattedData.map(
+        ({title, topic, author, body, created_at, votes, article_img_url}) => {
+          
+          return [title, topic, author, body, created_at, votes, article_img_url]
+        })
+
+      const insertArticleStr = format(
+        `INSERT into articles
+        (title, topic, author, body, created_at, votes, article_img_url)
+        VALUES %L
+        RETURNING *;`
+      , cleanedArticleData);
+      return db.query(insertArticleStr)
+    })
+    .then((newArticleData) => {
+      const formattedData = commentData.map(convertTimestampToDate)
+
+      const titleToArticleID = {}
+      newArticleData.rows.forEach((article) => {
+        titleToArticleID[article.title] = article.article_id;
+      })
+
+      const cleanedCommentData = formattedData.map(
+        ({article_title, body, votes, author, created_at}) => {
+          return [titleToArticleID[article_title], body, votes, author, created_at]
+        })
+
+      const insertCommentStr = format(
+        `INSERT into comments
+        (article_id, body, votes, author, created_at)
+        VALUES %L
+        RETURNING *;`
+      , cleanedCommentData);
+      return db.query(insertCommentStr)
+
+    })
+    
+    ; 
 };
 
 module.exports = seed;
